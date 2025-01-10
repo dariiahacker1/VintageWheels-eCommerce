e51dd10bf7b94150f366323880a9fa0928fd6e79
Dariia Shumeiko <dariia.shumeiko1@gmail.com>
2025-01-10 15:30:01 +0200

    Delete backend directory

--- backend/src/main/java/com/dealership/vintagewheels/repository/FAQRepository.java
+++
@@ -1,8 +0,0 @@
-package com.dealership.vintagewheels.repository;
-
-import com.dealership.vintagewheels.model.FAQ;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FAQRepository extends JpaRepository<FAQ, Long> {}
